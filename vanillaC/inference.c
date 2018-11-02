da5a52dd69ca7b2908c6545b0b0d7393890620d6
Rishikanth <rishi@Rishikanths-MacBook-Pro.local>
2018-11-02 03:16:05 -0700

    interval search implemented

--- vanillaC/inference.c
+++ vanillaC/inference.c
@@ -15,12 +15,35 @@ int whichInterval(float *partitions, int length, float val) {
 				interval = 2;
 			else
 				interval = 1;
-		default:
 			break;
+		default:
+			interval = binSearch(partitions, length, val);
+
 	}
 	return interval;
 }
 
 int binSearch(float *partitions, int length, float val) {
-	return 0;
+	int low, high, mid, i1, i2, interval;
+	
+	low = 0;
+	high = length - 1;
+
+	while (low <= high) {
+		mid = (low + high) >> 1;
+		i1 = partitions[mid];
+		i2 = partitions[mid + 1];
+
+		if (i1 <= val && val < i2)
+			return (mid + 1);
+		else if (val < i1)
+			high = mid - 1;
+		else
+			low = mid + 1;
+	}
+
+	if (high < 0)
+		return 0;
+	else
+		return length;
 }
--- vanillaC/test.c
+++ vanillaC/test.c
@@ -3,6 +3,10 @@
 
 int main()
 {
-	float a[2] = {2.33, 4.55};
-	whichInterval(a, 2, 3);
+	float a[4] = {-2.33, 4.65, 8.77, 23};
+	printf("4: %d\n", whichInterval(a, 4, 7));
+	printf("3: %d\n", whichInterval(a, 4, 3));
+	printf("11: %d\n", whichInterval(a, 4, 11));
+	printf("70: %d\n", whichInterval(a, 4, 70));
+	printf("-7: %d\n", whichInterval(a, 4, -7));
 }
