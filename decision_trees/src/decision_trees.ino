3fef502330fc6559f1241bf7b05e3894e3258921
Rishikanth <rishi@Rishikanths-MacBook-Pro.local>
2018-11-09 02:49:35 -0800

    adding code for testing on large data from sdcard

--- decision_trees/src/decision_trees.ino
+++ decision_trees/src/decision_trees.ino
@@ -6,8 +6,11 @@
  */
 #include "inference.h"
 #define LED D7
+#define CS_PIN SS
 
 int state = 0;
+SdFat SD;
+File file;
 float a[9] = {6.4983678,  0.88696843, 0.59811729, -0.45599312, 62240.84, 13285.165, 53057.996, 8.9070396, 8.7032156};
 
 SYSTEM_MODE(MANUAL);
@@ -19,21 +22,44 @@ void setup() {
 
 
   Serial.begin();
+
+  while (!Serial) {
+    SysCall::yield();
+  }
+  Serial.println("Type any character to start");
+  while (Serial.read() <= 0) {
+    SysCall::yield();
+  }
+  // Initialize the SD.
+  if (!SD.begin(CS_PIN)) {
+    errorHalt("begin failed");
+  }
+
+  file = SD.open("feat.txt", O_READ);
+  if (!file) errorHalt("open failed");
+
+  Serial.println("Starting read ...");
+  processData(file);
+  Serial.println("done");
+
+  file.close();
+
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-  digitalWrite(LED, (state) ? HIGH : LOW);
-  state = !state;
-  Serial.print("Test 1: ");
-  Serial.println(accInterval(a));
+  digitalWrite(LED, HIGH);
+  //digitalWrite(LED, (state) ? HIGH : LOW);
+  //state = !state;
+  //Serial.print("Test 1: ");
+  //Serial.println(accInterval(a));
   /*Serial.print("7: ");
   Serial.println(whichInterval(a, 4, 7));
   Serial.print("11: ");
   Serial.println(whichInterval(a, 4, 11));
   Serial.print("-7: ");
   Serial.println(whichInterval(a, 4, -7));*/
-  delay(1000);
+  //delay(1000);
 
 }
--- decision_trees/src/inference.h
+++ decision_trees/src/inference.h
@@ -1,3 +1,7 @@
+#include "application.h"
+#include "SdFat.h"
+
+#define errorHalt(msg) {Serial.println(F(msg)); SysCall::halt();}
 #define numsplits 9
 
 extern const float splits[][2];
@@ -7,3 +11,5 @@ void calcProd();
 int whichInterval(float *, int, float);
 int binSearch(float *, int, float);
 int accInterval(float *);
+size_t readField(File* file, char* str, size_t size, const char* delim); 
+void processData(File file);
