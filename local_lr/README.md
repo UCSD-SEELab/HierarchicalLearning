aad2a5b62a1098571c984d5218b5fbe6783ef7fe
Orienfish <x1yu@ucsd.edu>
2018-11-09 13:05:22 -0800

    runable lr and nn

--- local_lr/README.md
+++ local_lr/README.md
@@ -1,6 +1,5 @@
-# Neural Network (2 Hidden Layers)
-
-The test results in [another folder](https://github.com/Orienfish/photon/tree/master/mqtt_server) show the time consumtion in reading and computation respectively.
+# Linear Regression
+Implementation of linear regression on photon with data sent through MQTT. Time measurements are took in the program. 
 
 ## MQTT Library
 The MQTT library refers to contributions in this [repo](https://github.com/hirotakaster/MQTT)
@@ -11,7 +10,7 @@ To compile, flash, monitor from serial, you need the [Particle CLI](https://docs
 ## Run
 To run the code, you can simply connect your photon and download the code by
 ```
-particle flash --usb nn.bin
+particle flash --usb lr.bin
 ```
 and see test results through USB serial by
 ```
@@ -19,8 +18,5 @@ particle serial monitor
 ```
 To compile the code, use
 ```
-particle compile p --saveTo nn.bin
+particle compile p --saveTo lr.bin
 ```
-
-## Test Result
-Suppose N_1 = N_2 = N, BATCH_LEN = 1, the maximum N we can have is 90. Letting N = 95 will crash down.
--- local_lr/local_lr.ino
+++ local_lr/local_lr.ino
@@ -1,23 +1,19 @@
 /*
- * Project: Establish MQTT connection with RPi and send data after running
- *     basic neural network. Use fixed sample data.
+ * Project: Establish MQTT connection with RPi and send data after run linear regression
+ * Suppose necessary files are alreay written on the SD card:
+ *     data.txt (extracted_subject1.txt)
  * Author: Xiaofan Yu
- * Date: 11/7/2018
+ * Date: 11/2/2018
  */
 #include "application.h"
 #include "MQTT.h"
 
 #define MAX_RAND_NUM 1000000 // maximum number in random()
-#define MAX_INFO_LENGTH 100 // maximum length of sending info, e.g. time consumption
+#define MAX_INFO_LENGTH 100 // maximum length of sending info, e.g. bw and sample rate
 #define SEND_TIMES 1000 // the times that we do sending experiments
 #define FEATURE_NUM 12 // number of features, linear regression input
 #define CLASSES_NUM 5 // number of classes, linear regression output
-// size of neural network
-#define N_IN FEATURE_NUM // nodes as input
-#define N_OUT CLASSES_NUM // nodes as output
-#define N_1 80 // nodes in the first layer
-#define N_2 80 // nodes in the second layer
-#define BATCH_LEN 2 // the number of input samples for one time
+#define BATCH_LEN 200 // the number of input samples for one time
 
 // const variables in experiments
 const float sample_data[FEATURE_NUM] = {1.5663333, 2.0231032, 4.368568, -0.7739354,
@@ -26,53 +22,32 @@ const unsigned int copy_size = FEATURE_NUM * sizeof(float);
 
 // global variables for MQTT
 byte server[] = { 137,110,160,230 }; // specify the ip address of RPi
-// ip, port, keepalive, callback, maxpacketsize
-MQTT client(server, 1883, 60, NULL, max(SEND_TIMES*BATCH_LEN, MAX_INFO_LENGTH)+10);
+ // ip, port, keepalive, callback, maxpacketsize
+MQTT client(server, 1883, 60, NULL, max(BATCH_LEN, MAX_INFO_LENGTH)+10);
 
-// nodes for neural networks
-float nn_in[BATCH_LEN][N_IN], nn_out[BATCH_LEN][N_OUT]; // input and output nodes
-float nn_l1[BATCH_LEN][N_1], nn_l2[BATCH_LEN][N_2]; // middle layer nodes in nn
-float nn_w1[N_IN][N_1], nn_w2[N_1][N_2], nn_w3[N_2][N_OUT]; // weights between layers
+// global array to store float data for linear regression
+float lr_para[FEATURE_NUM][CLASSES_NUM]; // parameters for linear regression
+float lr_in[BATCH_LEN][FEATURE_NUM], lr_out[BATCH_LEN][CLASSES_NUM];
 // glabal array to store send batch
-uint8_t batch_data[SEND_TIMES*BATCH_LEN+10]; // send batch
+uint8_t batch_data[BATCH_LEN+10]; // send batch
 unsigned int len_of_batch = 0; // len of send batch
-// global array to store time measurements
+// global array to store assistant info
 char time_measure[MAX_INFO_LENGTH];
 
 /*************************************************************************
- * NN functions
+ * linear regression related code
  ************************************************************************/
 /*
- * init_weights
+ * init_lr_parameter
  */
-void init_nn_weights() {
+void init_lr_parameter() {
 	long randNum;
-	for (int i = 0; i < N_IN; ++i)
-		for (int j = 0; j < N_1; ++j) {
+	for (uint8_t i = 0; i < FEATURE_NUM; ++i)
+		for (uint8_t j = 0; j < CLASSES_NUM; ++j) {
 			randNum = random(0, MAX_RAND_NUM);
-			nn_w1[i][j] = ((float)randNum) / MAX_RAND_NUM;
+			lr_para[i][j] = ((float)(randNum - MAX_RAND_NUM)) / MAX_RAND_NUM;
+			// Serial.printf("%f ", para_num[i][j]);
 		}
-	for (int i = 0; i < N_1; ++i)
-		for (int j = 0; j < N_2; ++j) {
-			randNum = random(0, MAX_RAND_NUM);
-			nn_w2[i][j] = ((float)randNum) / MAX_RAND_NUM;
-		}
-	for (int i = 0; i < N_2; ++i)
-		for (int j = 0; j < N_OUT; ++j) {
-			randNum = random(0, MAX_RAND_NUM);
-			nn_w3[i][j] = ((float)randNum) / MAX_RAND_NUM;
-		}
-}
-
-/*
- * ReLU activation. Read continuously!
- */
-inline void ReLU(float *x, int row, int col) {
-	for (int i = 0; i < row; ++i) {
-		float *line = x + i * col; // record to save energy
-		for (int j = 0; j < col; ++j)
-			*(line + j) > 0? 1: *(line + j) = 0; // do nothing or make it zero
-	}
 }
 
 /*
@@ -103,31 +78,6 @@ void matrix_multiply(float *m1, int row_m1, int col_m1,
 }
 
 /*
- * nn: in -> nn -> out
- * "in" is feature_num * batch_num, "out" is classnum*batch_num
- */
-void nn(float *in, int in_row, int in_col,
-	    float *out, int out_row, int out_col) {
-	// check whether the rows of input and output equal
-	if (in_row != out_row || in_col != N_IN || out_col != N_OUT)
-		return;
-	matrix_multiply(in, in_row, in_col,
-					(float *)nn_w1, N_IN, N_1,
-					(float *)nn_l1, in_row, N_1);
-	ReLU((float *)nn_l1, in_row, N_1);
-
-	matrix_multiply((float *)nn_l1, in_row, N_1,
-					(float *)nn_w2, N_1, N_2,
-					(float *)nn_l2, in_row, N_2);
-	ReLU((float *)nn_l2, in_row, N_2);
-
-	matrix_multiply((float *)nn_l2, in_row, N_2,
-					(float *)nn_w3, N_2, N_OUT,
-					out, out_row, out_col);
-	ReLU(out, out_row, out_col);
-}
-
-/*
  * pack: pack out to batch_data. 
  * out_col should equal to N_OUT as we read continuously!
  */
@@ -147,19 +97,6 @@ void pack(float *out, int out_row, int out_col) {
 		// Serial.printf("max index is %d (%d)\r\n", max_index, len_of_batch);
 	}
 }
-
-/*
- * print_matrix
- */
-/*inline void print_matrix(float *m, int row, int col) {
-	for (int i = 0; i < row; ++i) {
-		for (int j = 0; j < col; ++j)
-			Serial.printf("%f ", *(m+i*col+j));
-		Serial.printf("\r\n");
-	}
-	Serial.printf("\r\n");
-}*/
-
 /*************************************************************************
  * Other settings for local connector
  ************************************************************************/
@@ -167,7 +104,7 @@ void pack(float *out, int out_row, int out_col) {
  * read_matrix - fit the sample data into the matrix
  */
 void read_matrix(float *in, int in_row, int in_col) {
-	if (in_col != N_IN) // check whether in_col matches with feature num
+	if (in_col != FEATURE_NUM) // check whether in_col matches with feature num
 		return;
 	for (int i = 0; i < in_row; ++i) {
 		memcpy(in, sample_data, copy_size);
@@ -180,7 +117,7 @@ void read_matrix(float *in, int in_row, int in_col) {
  */
 void setup() {
 	Serial.begin(115200);
-	init_nn_weights(); // init nn weights
+	init_lr_parameter(); // init lr parameters
 
 	// variables used in time testing
 	unsigned long comp_start, total_comp_time; // to record the total time for calculating avg
@@ -191,28 +128,30 @@ void setup() {
 	client.connect("photon", "xiaofan", "0808", 0, client.QOS2, 0, 0, true, client.MQTT_V311);
 	Serial.println("Connect!");
 
-	total_comp_time = len_of_batch = 0;
+	total_comp_time = 0;
 	// run the experiments for SEND_TIMES times
 	for (int send_num = 0; send_num < SEND_TIMES; ++send_num) {
+		len_of_batch = 0;
 		// read sample_rate lines
-		read_matrix((float *)nn_in, BATCH_LEN, N_IN);
+		read_matrix((float *)lr_in, BATCH_LEN, FEATURE_NUM);
 		// do local computation
 		comp_start = millis();
-		// compute nn for this input matrix
-		nn((float *)nn_in, BATCH_LEN, N_IN, (float *)nn_out, BATCH_LEN, N_OUT);
+		// compute lr for this input matrix
+		matrix_multiply((float *)lr_in, BATCH_LEN, FEATURE_NUM, 
+			(float *)lr_para, FEATURE_NUM, CLASSES_NUM,
+			(float *)lr_out, BATCH_LEN, CLASSES_NUM);
 		// pack the result into send batch
-		pack((float *)nn_out, BATCH_LEN, N_OUT);
+		pack((float *)lr_out, BATCH_LEN, CLASSES_NUM);
+		// publish computation result
+		if (client.isConnected()) {
+			client.publish("data", batch_data, len_of_batch, false, client.QOS2, false, NULL);
+			while (!client.loop_QoS2()); // block and wait for pub done
+		}
+		Serial.printf("publish %d bytes successfully!\r\n", len_of_batch);
 		// update total time
 		total_comp_time += millis() - comp_start; // cumulative add
 		Serial.println(send_num);
 	}
-	// publish computation result
-	if (client.isConnected()) {
-		client.publish("data", batch_data, len_of_batch, false, client.QOS2, false, NULL);
-		while (!client.loop_QoS2()); // block and wait for pub done
-	}
-	Serial.printf("publish %d bytes successfully!\r\n", len_of_batch);
-
 	// compute the avg time consumption and send it
 	comp_avg = (float)total_comp_time / SEND_TIMES;
 	sprintf(time_measure, "comp avg:%f\r\n", comp_avg);
